87c7ad336bb30eb10cb85cd34587500fcdebec97
UifeleanAuroraSofia <uifelean.fl.aurora@student.utcluj.ro>
2025-02-23 22:48:36 +0200

    update database connections

--- FoodScannerAPI/settings.py
+++ FoodScannerAPI/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'FoodScannerAPI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME', 'FoodScanner'),
-        'USER': os.environ.get('DB_USER', 'postgres'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', 'admin'),
-        'HOST': os.environ.get('DB_HOST', 'foodscannerapi.onrender.com'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
